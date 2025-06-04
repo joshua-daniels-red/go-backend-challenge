d1744643905437a4aed62ef0e4fb010b80496041
Joshua D <joshua.daniels@redspace.com>
2025-06-04 12:16:53 -0300

    adding prometheus

---
+++ ch-6/internal/stream/metrics.go
@@ -0,0 +1,53 @@
+package stream
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	EventsConsumedFromStream = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "events_consumed_from_stream_total",
+			Help: "Number of events consumed from the Wikipedia stream",
+		},
+	)
+
+	EventsProducedToRedpanda = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "events_produced_to_redpanda_total",
+			Help: "Number of events produced to Redpanda",
+		},
+	)
+
+	EventsConsumedFromRedpanda = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "events_consumed_from_redpanda_total",
+			Help: "Number of events consumed from Redpanda",
+		},
+	)
+
+	EventsProcessedSuccessfully = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "events_processed_successfully_total",
+			Help: "Number of events processed successfully",
+		},
+	)
+
+	EventsFailedToProcess = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "events_failed_to_process_total",
+			Help: "Number of events that failed during processing",
+		},
+	)
+)
+
+// RegisterMetrics registers all custom Prometheus metrics
+func RegisterMetrics() {
+	prometheus.MustRegister(
+		EventsConsumedFromStream,
+		EventsProducedToRedpanda,
+		EventsConsumedFromRedpanda,
+		EventsProcessedSuccessfully,
+		EventsFailedToProcess,
+	)
+}
